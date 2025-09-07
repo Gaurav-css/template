43a631adf5b213dc5adb5b56df38a4ff79648ce1
Gaurav-css <gky895522@example.com>
2025-09-07 22:30:03 +0530

    trying pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import Navbar from "@/components/Navbar"
 // import HeroSection from "@/components/HeroSection"
+import Testimonials from "@/components/Testimonials"  
+import Layout from "@/components/Layout"
 
 export default function HomePage() {
   return (
     <div>
       {/* Navbar */}
       <Navbar />
+      <Layout />
+      <Testimonials />
 
-      {/* Hero Section */}
-      {/* <HeroSection /> */}
+      
+      
     </div>
   )
 }
