1f136d200778676569f38e4ec1e64c0cf1656208
Riyakuila <riyakuila1006@gmail.com>
2025-09-19 10:17:04 +0530

    Remove gap between the Feature section and Testimonial section

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -151,7 +151,7 @@ export default function App() {
 
 
   return (
-    <div className="bg-gray-50 dark:bg-[#1A1A1A] text-gray-800 dark:text-gray-200">
+    <div className="bg-gray-50 dark:bg-[#1A1A1A] text-gray-800 dark:text-gray-200 -mb-20">
       <div className="container mx-auto max-w-7xl px-3 sm:px-4 md:px-6 lg:px-8">
         <div className="grid lg:grid-cols-2 gap-12 lg:gap-24">
 
